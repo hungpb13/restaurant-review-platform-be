203ea72d5161305594ecafaaea38ceb9235dee5b
hungpb13 <hungpb13@gmail.com>
2025-09-15 20:40:51 +0700

    Photo Retrieval Endpoint

--- src/main/java/com/dev/restaurant/controllers/PhotoController.java
+++ src/main/java/com/dev/restaurant/controllers/PhotoController.java
@@ -5,10 +5,12 @@ import com.dev.restaurant.domain.entities.Photo;
 import com.dev.restaurant.mappers.PhotoMapper;
 import com.dev.restaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -25,5 +27,19 @@ public class PhotoController {
     ) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory
+                                        .getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }

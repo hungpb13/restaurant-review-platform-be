f8b044b8b318fe094b4e3af4499687b4bc4d4ac0
hungpb13 <hungpb13@gmail.com>
2025-09-16 10:39:54 +0700

    Restaurant Search Service

--- src/main/java/com/dev/restaurant/services/RestaurantService.java
+++ src/main/java/com/dev/restaurant/services/RestaurantService.java
@@ -2,7 +2,18 @@ package com.dev.restaurant.services;
 
 import com.dev.restaurant.domain.entities.Restaurant;
 import com.dev.restaurant.domain.requests.RestaurantCreateUpdateRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RestaurantService {
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurants(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
 }
